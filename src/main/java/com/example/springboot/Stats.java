7c89594b6d99903e3fc23330c8a4d9cfe7267339
obinna Emelumadu <39995754+obinnaEmelumadu@users.noreply.github.com>
2020-02-16 19:34:55 +0100

    First commit

---
+++ src/main/java/com/example/springboot/Stats.java
@@ -0,0 +1,39 @@
+package com.example.springboot;
+
+import java.util.Map;
+
+public class Stats {
+    private final boolean success;
+    private final int start;
+    private final int limit;
+    private final int size;
+    private final Map<String, Object> payload;
+
+    public Stats(
+        final int start, 
+        final int limit, 
+        final int size, 
+        final Map<String, Object> payload) {
+            this.success = true;
+            this.start = start;
+            this.limit = limit;
+            this.size = size;
+            this.payload = payload;
+        }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public Map<String, Object> getPayload() {
+        return payload;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
